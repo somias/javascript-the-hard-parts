2b666cff1736a95a96ac71a3397054710f38fb1c
milos.savkovic <milos.savkovic@nites.eu>
2024-10-29 14:28:12 +0100

    Complete callback challenges from 6 to 8.

--- callbacks/index.js
+++ callbacks/index.js
@@ -67,4 +67,50 @@ function mapWith(array, callback) {
     return newArray;
 }
 
-console.log(mapWith([1, 2, 3], addTwo));
+// console.log(mapWith([1, 2, 3], addTwo));
+
+// Challenge 6
+// Create a function called reduce that takes an array and reduces the elements to a single value.
+// For example, it can sum all the numbers, multiply them, or any operation that you can put into a function.
+
+function reduce(array, callback, startingValue) {
+    let initialValue = startingValue;
+    for (let i = 0; i < array.length; i++) {
+        initialValue = callback(initialValue, array[i])
+    }
+
+    return initialValue;
+}
+
+const nums = [4, 1, 3];
+const add = function(a, b) { return a + b; }
+// console.log(reduce(nums, add, 0));   //-> 8
+
+// Challenge 7
+// Construct a function intersection that takes in an array of arrays,
+// compares the inner arrays, and returns a new array with elements found in all of them. BONUS: Use reduce!
+
+function intersection(arrays) {
+    return arrays.reduce((acc, curr) => {
+        return curr.filter(el => acc.includes(el))
+    })
+}
+
+// console.log(intersection([[5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]]));
+// should log: [5, 15]
+
+// Challenge 8
+// Construct a function union that takes in an array of arrays,
+// compares the inner arrays, and returns a new array that contains all elements.
+// If there are duplicate elements, only add it once to the new array.
+// Preserve the order of the elements starting from the first element of the first array. BONUS: Use reduce!
+
+function union(arrays) {
+    return arrays.reduce((acc, curr) => {
+        const elements = curr.filter(el => !acc.includes(el));
+        return acc.concat(elements);
+    });
+}
+
+console.log(union([[5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]]));
+// should log: [5, 10, 15, 88, 1, 7, 100]
